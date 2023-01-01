800db474b1ca7620e6bdb02e2cfb4f5617c17342
tyToplu <109238953+tyToplu@users.noreply.github.com>
2023-01-01 14:06:48 +0300

    Update ODEV5'ten sonrasi.sql

--- ODEV5'ten sonrasi.sql
+++ ODEV5'ten sonrasi.sql
@@ -108,3 +108,30 @@ WHERE birthday = '2021-12-19';
 DELETE FROM employee
 WHERE name = 'Karita';
 
+ODEV 9
+--1city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) 
+--isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+--2customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN 
+--sorgusunu yazınız.
+--3customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN 
+--sorgusunu yazınız.
+
+--1--
+SELECT CITY, COUNTRY FROM CITY 
+INNER JOIN ON CITY.CITY_ID = COUNTRY_ID;
+--2--
+SELECT payment_id,first_name,last_name FROM payment INNER JOIN customer ON payment.customer_id = customer.customer_id;
+--3--
+SELECT rental_id,first_name,last_name FROM rental INNER JOIN customer ON customer.customer_id = rental.customer_id;
+
+
+
+
+
+
+
+
+
+
+
+
