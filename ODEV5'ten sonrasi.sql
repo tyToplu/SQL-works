bfb0cbce4cb009b7116650eaa2093b056697dba1
tyToplu <109238953+tyToplu@users.noreply.github.com>
2023-01-01 14:21:02 +0300

    Update ODEV5'ten sonrasi.sql

--- ODEV5'ten sonrasi.sql
+++ ODEV5'ten sonrasi.sql
@@ -125,9 +125,26 @@ SELECT payment_id,first_name,last_name FROM payment INNER JOIN customer ON payme
 SELECT rental_id,first_name,last_name FROM rental INNER JOIN customer ON customer.customer_id = rental.customer_id;
 
 
+ODEV 10
+--1city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+--2customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN 
+--sorgusunu yazınız.
+--3customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN 
+--sorgusunu yazınız.
 
-
-
+--1--
+SELECT city, country FROM city
+LEFT JOIN country ON country.country_id = city.country_id;
+--2--
+SELECT payment_id,first_name,last_name
+FROM payment
+RIGHT JOIN customer 
+ON payment.customer_id = customer.customer_id
+--3--
+SELECT rental.rental_id, customer.first_name, customer.last_name
+FROM rental
+FULL JOIN customer
+ON rental.customer_id = customer.customer_id;
 
 
 
