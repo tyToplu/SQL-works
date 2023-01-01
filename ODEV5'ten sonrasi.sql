7f5f929c893936c92f884df30db331c0fe7ffe76
tyToplu <109238953+tyToplu@users.noreply.github.com>
2023-01-01 14:50:21 +0300

    Update ODEV5'ten sonrasi.sql

--- ODEV5'ten sonrasi.sql
+++ ODEV5'ten sonrasi.sql
@@ -171,9 +171,41 @@ SELECT first_name FROM actor EXCEPT SELECT first_name FROM customer;
   --4.3--
   SELECT first_name FROM actor EXCEPT ALL SELECT first_name FROM customer;
 
-
-
-
+ODEV 12
+--1film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+--2film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+--3film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+--4payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız
+--1--
+SELECT COUNT(*) FROM FILM
+WHERE LENGTH >(
+SELECT AVG(LENGHT) FROM FILM
+)
+--2-
+SELECT COUNT(*) 
+FROM film
+WHERE rental_rate = 
+(
+SELECT MAX(rental_rate)
+FROM film
+);
+--3-
+SELECT *
+FROM film
+WHERE rental_rate = 
+(
+SELECT MIN(rental_rate)
+FROM film
+)
+AND
+replacement_cost = 
+(
+SELECT MIN(replacement_cost)
+FROM film
+)
+--4-
+SELECT customer_id, SUM(amount)  FROM payment GROUP BY customer_id ORDER BY SUM DESC LIMIT 5;
+Footer
 
 
 
